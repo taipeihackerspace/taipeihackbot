3110909af5bca8a47d407e510f48c05f6fd07b7c
Gergely Imreh <imrehg@gmail.com>
2014-02-06 22:48:35 +0800

    Initial commit

---
+++ bot.js
@@ -0,0 +1,29 @@
+// Create the configuration
+var config = {
+    channels: ["#taipeihack"],
+    server: "irc.freenode.net",
+    botName: "taipeihackbot",
+    admins: ["imrehg"]
+};
+
+// Get the lib
+var irc = require("irc");
+
+// Create the bot name
+var bot = new irc.Client(config.server, config.botName, {
+    channels: config.channels
+});
+
+// Listen for joins
+bot.addListener("join", function(channel, who) {
+    // Welcome them in!
+    if (who === config.botName) {
+	return;
+    }
+
+    bot.say(channel, who + "...heya...welcome to the (virtual) Hackerspace!");
+    if (config.admins.indexOf(who) >= 0) {
+	bot.send('MODE', channel, '+o', who);
+    }
+
+});
