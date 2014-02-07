41c2ff85f7a293a6898019eddf86eb208c6b0926
Gergely Imreh <imrehg@gmail.com>
2014-02-07 10:08:14 +0800

    add very basic function and message parsing to the bot

--- bot.js
+++ bot.js
@@ -30,6 +30,24 @@ bot.addListener("join", function(channel, who) {
 
 bot.addListener('message', function (from, to, message) {
     console.log(from + ' => ' + to + ': ' + message);
+    var mp = message.split(' ');
+    if (mp[0] === config.botName) {
+	switch (mp[1]) {
+	    case 'help':
+	       bot.say(to, 'Got to figure it out yourself, I guess...');
+	       break;
+	    case 'flip':
+	       var outcome = (Math.random() > 0.5) ? 'heads' : 'tails';
+	       bot.say(to, 'It\'s '+outcome);
+	       break;
+	    case 'roll':
+	       var outcome = Math.floor(Math.random() * 6) + 1;
+	       bot.say(to, 'Rolled a '+outcome);
+	       break;
+	    default:
+	       bot.say(to, "Hmm?");
+	}
+    }
 });
 
 bot.addListener('pm', function (from, message) {
